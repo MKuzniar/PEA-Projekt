082d7d601058f2efac9d6e37f8fa33b0b6e644b3
threeyedbird <49250750+threeyedbird@users.noreply.github.com>
2020-10-24 10:45:18 +0200

    Adding new functionalities
    Generating all the possible routes, counting their cost and highlighting the best one

---
+++ Zadanie projektowe 1/Update - 24.10.2020r/algorytmBF.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+void generujPermutacje(int liczba_miast, vector<int> miacierz, int koszt_minimalny);
+
+void wyswietlWektor(vector<int> permutacje);
+
+void obliczKoszt(vector<int> permutacje, vector<int> macierz, int koszt_minimalny);
