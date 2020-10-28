d59be68772968c5cf512e795fd64353cd22fb1ba
threeyedbird <49250750+threeyedbird@users.noreply.github.com>
2020-10-28 21:06:45 +0100

    Final touches
    Refining names

---
+++ Zadanie projektowe 1/Update - 28.10.2020r/algorytmDP.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int algorytmDP(int maska, int aktualne_miasto, int n, vector<int> v); //rekurencyjna funkcja wyznaczajaca najkrotsza droge miedzy miastami 
